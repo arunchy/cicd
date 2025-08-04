8ade894bf48545761f3d4ffbcff7d8d2c1b0d0f4
arunchy <arunchy600@gmail.com>
2025-08-04 12:18:23 +0545

    trigger cicd

---
+++ backend/server.js
@@ -0,0 +1,24 @@
+// backend/index.js
+
+const express = require("express");
+const app = express();
+
+// Middleware to parse JSON
+app.use(express.json());
+
+// Basic API route
+app.get("/api", (req, res) => {
+  res.json({ message: "Hello from the Express API!" });
+});
+
+app.get("/",(req,res)=>{
+  console.log(req.body);
+  res.json({message:"Get request resolve from backend.."})
+})
+
+// Port must match Nginx proxy_pass
+const PORT = 5000;
+
+app.listen(PORT,'0.0.0.0', () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
